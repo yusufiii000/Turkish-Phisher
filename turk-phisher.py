046a34b3204aa773362b69934f424f07827c74c6
yusufiii000 <158528545+yusufiii000@users.noreply.github.com>
2024-02-14 13:56:40 +0300

    turk-phisher.py

---
+++ turk-phisher.py
@@ -0,0 +1,114 @@
+print('MOD BASLATILIYOR HAHAHAHAHAHA')
+
+print("")
+
+import time
+
+def loading_animation():
+    animation = "|/-\\"
+    for i in range(25):
+        time.sleep(0.1)
+        print(f"\rLoading {animation[i % len(animation)]}", end='', flush=True)
+
+loading_animation()
+print('')
+print('')
+print('')
+print("Moduma Hoş Geldiniz¡¡¡")
+print('')
+print('')
+
+_author_='Yusufi'
+_license_='GPL'
+_version_='Very start'
+
+print('')
+
+print('Mod Amacı')
+
+print('')
+
+import time
+
+def loading_animation():
+    animation = "YUSUFI--YAPIMSSS"
+    for i in range(25):
+        time.sleep(0.3)
+        print(f"\rLoading {animation[i % len(animation)]}", end='', flush=True)
+
+loading_animation()
+
+print('')
+print('')
+
+print('Phisher e Hoş Geldiniz.Çalışmalırımız son hızıyla sürmektedir...')
+print('')
+
+print('Ekran Temizleniyor....')
+
+print('')
+print('')
+
+import time
+
+def loading_animation():
+    animation = "Temizleniyor-"
+    for i in range(25):
+        time.sleep(0.3)
+        print(f"\rLoading {animation[i % len(animation)]}", end='', flush=True)
+
+loading_animation()
+
+import os 
+os.system('clear')
+
+print('')
+print('TOOL BASLATILIYOR...')
+print('')
+print('')
+import time
+
+def loading_animation():
+    animation = "BASLIYOR--"
+    for i in range(25):
+        time.sleep(0.3)
+        print(f"\rLoading {animation[i % len(animation)]}", end='', flush=True)
+
+loading_animation()
+
+print('')
+print('')
+
+a=int(input('Başlatmak İçin Parolayı Giriniz == '))
+print('')
+print('')
+
+if a==31626971 :print("Calışmalarımız Sürüyor...")
+else :print('Yanlış Parola')
+
+print('')
+print('')
+
+
+if a==1 :print('Çıkış Yapılıyor...')
+print('')
+print('')
+
+if a==31626971 :print('Çıkış Yapılıyor...')
+print('')
+print('')
+
+
+def loading_animation():
+    animation = "Çıkılıyor--"
+    for i in range(25):
+        time.sleep(0.3)
+        print(f"\rLoading {animation[i % len(animation)]}", end='', flush=True)
+
+loading_animation()
+
+import os
+os.system('clear')
+
+
+
